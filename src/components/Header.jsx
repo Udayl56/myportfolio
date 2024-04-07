d618e5b31f4b562adc3ad58dfa79abda3842dcf1
Uday Ladhi <105300620+Udayl56@users.noreply.github.com>
2024-04-07 12:02:39 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ export function Header() {
             <div className="row p-2 d-flex justify-content-center align-items-center " >
 
                 <div className=' col-6 col-md-3  p-sm-2  d-md-flex d-flex align-items-center'>
-                    <a target="_blank" href="https://github.com/Udayl56" className='p-2 text-white text-decoration-none fs-4 '> <FontAwesomeIcon icon={faGithub} /><span className=' p-1'> Udayl56</span></a>
+                    <a target="_blank" href="https://github.com/Udayl56" rel="noreferrer noopener" className='p-2 text-white text-decoration-none fs-4 '> <FontAwesomeIcon icon={faGithub} /><span className=' p-1'> Udayl56</span></a>
                 </div>
 
                 <div className='d-sm-none d-none col-md-6 d-md-flex flex-md-row gap-md-5 gap-sm-1 justify-content-center d-flex align-items-center'>
@@ -29,7 +29,7 @@ export function Header() {
                 <div className="  col-6 col-md-3 p-md-4 p-sm-2 p-1 d-flex align-items-center justify-content-end ">
 
 
-                    <a target="_blank" href="https://www.linkedin.com/in/uday-ladhi-14a10b244/" className="ps-sm-4 pe-sm-4 p-2 bg rounded-pill text-white text-decoration-none ">
+                    <a target="_blank" href="https://www.linkedin.com/in/uday-ladhi-14a10b244/" rel="noreferrer noopener" className="ps-sm-4 pe-sm-4 p-2 bg rounded-pill text-white text-decoration-none ">
                         <FontAwesomeIcon icon={faLinkedin} /><span className=' p-1'> Connect</span> </a>
                 </div>
 
