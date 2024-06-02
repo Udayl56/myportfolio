311d5e639f4e59e2841e1cf8d873eb36722e2621
udayl59 <udayladhi@gmail.com>
2024-06-02 10:49:33 +0530

    new project added

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -6,7 +6,9 @@ import projectimg_3 from "../assets/resume-builder.png"
 import projectimg_4 from "../assets/birthday-wish-notifier.png"
 import projectimg_5 from '../assets/diabetesPrediction.png'
 import projectimg_6 from '../assets/stockdata.png'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import projectimg_7 from '../assets/js-coding-stack.png'
+import projectimg_8 from '../assets/contact-managment.png'
+
 import { WorkCard } from './WorkCard';
 
 
@@ -18,6 +20,8 @@ export function Work() {
             <h4 className=" fw-bold p-4 text-cg" >Explore My Latest Works</h4>
             <div className="row d-flex-flex-wrap">
 
+                <WorkCard title={"Js Coding stack"} src={projectimg_7} projectlink={'https://js-coding-stack-vite.vercel.app/'} />
+                <WorkCard title={"Contact Managment using redux toolkit"} src={projectimg_8} projectlink={'https://contact-managment-beta.vercel.app/'} />
                 <WorkCard title={"My portfolio"} src={projectimg_1} projectlink={'https://github.com/Udayl56/portfolio'} />
                 <WorkCard title={"Responsive Mockup"} src={projectimg_2} projectlink={'https://github.com/Udayl56/rapidQuest-Assignment-'} />
                 <WorkCard title={"Resume Builder Application"} src={projectimg_3} projectlink={'https://github.com/Udayl56/resume-builder'} />
